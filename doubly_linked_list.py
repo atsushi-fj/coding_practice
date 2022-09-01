77110fc865f2e7e1a6edf46c632c7a6c80fe1b3f
atsus-fj <atsushi061452@icloud.com>
2022-09-02 00:40:48 +0900

    メソッドの追加

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -77,19 +77,77 @@ class DoublyLinkedList(object):
             current_node = None
             return
 
+    def reverse_iterative(self) -> None:  # 逆順
+        previous_node = None
+        current_node = self.head
+        while current_node:
+            previous_node = current_node.prev
+            current_node.prev = current_node.next
+            current_node.next = previous_node
+            current_node = current_node.prev
+
+        if previous_node:  # ループを1回でもしたとき
+            self.head = previous_node.prev
+
+    def reverse_recursive(self) -> None:  # 逆順
+        def _reverse_recursive(current_node: Node) -> Optional[Node]:  # NoneかNodeがかえる
+            if not current_node:  # 何もないとき
+                return None  # self.head = None
+
+            previous_node = current_node.prev
+            current_node.prev = current_node.next
+            current_node.next = previous_node
+
+            if not current_node.prev:
+                return current_node
+
+            return _reverse_recursive(current_node.prev)  # current_node = current_node.prev
+
+        self.head = _reverse_recursive(self.head)
+
+    def sort(self) -> None:  # 昇順にソートする
+        if self.head is None:
+            return
+
+        current_node = self.head
+        while current_node:
+            next_node = current_node.next
+            while next_node:
+                if current_node.data > next_node.data:
+                    current_node.data, next_node.data = next_node.data, current_node.data
+                next_node = next_node.next
+            current_node = current_node.next
+
 
 if __name__ == '__main__':
     d = DoublyLinkedList()
-    d.append(1)
+    d.append(0)
+    d.append(5)
     d.append(2)
-    d.append(3)
-    d.insert(0)
+    d.insert(1)
+    d.insert(9)
+    d.print()
+
+    print('##############################')
+
+    d.reverse_iterative()
+    d.print()
+
+    print('##############################')
+
+    d.reverse_recursive()
     d.print()
 
     print('##############################')
 
     d.remove(2)
     d.print()
+
+    print('#############################')
+
+    d.sort()
+    d.print()
+
 
 
 
